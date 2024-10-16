90a87524bfdd2b596305077cfbfebea726eaf2f1
TSR <43693657+four-0-four@users.noreply.github.com>
2024-10-16 15:02:52 +0330

    Initial Commit

---
+++ LOTR Converter 18/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LOTR Converter 18
+//
+//  Created by Sina on 2024-10-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LOTR Converter 18/LOTR_Converter_18App.swift
@@ -0,0 +1,17 @@
+//
+//  LOTR_Converter_18App.swift
+//  LOTR Converter 18
+//
+//  Created by Sina on 2024-10-16.
+//
+
+import SwiftUI
+
+@main
+struct LOTR_Converter_18App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
